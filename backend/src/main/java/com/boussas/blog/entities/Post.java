ba89b8876452ab6759965d67c451dcedc2ae6c6a
Mohamed <mohamedboussas0@gmail.com>
2025-07-14 11:54:58 +0100

    Added Post and PostStatus entities

---
+++ backend/src/main/java/com/boussas/blog/entities/Post.java
@@ -0,0 +1,61 @@
+package com.boussas.blog.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "posts")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false,columnDefinition ="TEXT")
+    private String content;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostStatus status;
+    @Column(nullable = false)
+    private Integer readingTime;
+ //  private Integer views;
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id) && Objects.equals(title, post.title) && Objects.equals(content, post.content) && status == post.status && Objects.equals(readingTime, post.readingTime) && Objects.equals(createdAt, post.createdAt) && Objects.equals(updatedAt, post.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, content, status, readingTime, createdAt, updatedAt);
+    }
+
+    @PrePersist
+    public void create() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+    @PreUpdate
+    public void update() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
