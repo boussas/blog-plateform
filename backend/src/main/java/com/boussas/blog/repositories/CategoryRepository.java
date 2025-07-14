5337c4f1bd92ffe170edc5d3291799d34c52031b
Mohamed <mohamedboussas0@gmail.com>
2025-07-14 13:02:10 +0100

    Added Repos

---
+++ backend/src/main/java/com/boussas/blog/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.boussas.blog.repositories;
+
+import com.boussas.blog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<User, UUID> {
+}
